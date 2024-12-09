072a8f93f66f400aaf005020ba2afcf28b44ad16
thimanshu756 <himanshu1662.be22@gmail.com>
2024-12-10 00:48:50 +0530

    final push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-pink-50">
+      <p>Hello Baby</p>
+      <p>I love You </p>
+    </div>
+  );
+}
+
+export default App;
